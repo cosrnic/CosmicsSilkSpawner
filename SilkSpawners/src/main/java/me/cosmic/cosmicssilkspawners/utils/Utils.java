b44c4afb7b2b8e881c26a2e0c883372b4dd2ad6b
Cosmic-0022 <75631523+Cosmic-0022@users.noreply.github.com>
2021-02-08 19:20:06 +0000

    Update Utils.java

--- SilkSpawners/src/main/java/me/cosmic/cosmicssilkspawners/utils/Utils.java
+++ SilkSpawners/src/main/java/me/cosmic/cosmicssilkspawners/utils/Utils.java
@@ -3,7 +3,7 @@ package me.cosmic.cosmicssilkspawners.utils;
 import org.bukkit.ChatColor;
 
 public class Utils {
-
+    // only used for startup/shutdown - allows colour codes to be used
     public static String chat (String s) {
         return ChatColor.translateAlternateColorCodes('&', s);
     }
