2fd28d689153fc0e00c27f28a993498d7a021a4f
Cosmic-0022 <75631523+Cosmic-0022@users.noreply.github.com>
2021-03-12 08:40:21 +0000

    Update CosmicsSilkSpawners.java

--- SilkSpawners/src/main/java/me/cosmic/cosmicssilkspawners/CosmicsSilkSpawners.java
+++ SilkSpawners/src/main/java/me/cosmic/cosmicssilkspawners/CosmicsSilkSpawners.java
@@ -9,14 +9,14 @@ public final class CosmicsSilkSpawners extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        getServer().getConsoleSender().sendMessage(Utils.chat("&bCSSpawners &7>> &aPlugin Enabled!"));//prints enabled message to console
-        getServer().getPluginManager().registerEvents(new BlockBreakListener(), this);//lets the plugin read this file
-        getServer().getPluginManager().registerEvents(new CustomListener(), this);//lets the plugin read this file
+        getServer().getConsoleSender().sendMessage(Utils.chat("&bCSSpawners &7>> &aPlugin Enabled!"));
+        getServer().getPluginManager().registerEvents(new BlockBreakListener(), this);
+        getServer().getPluginManager().registerEvents(new CustomListener(), this);
 
     }
 
     @Override
     public void onDisable() {
-        getServer().getConsoleSender().sendMessage(Utils.chat("&bCSSpawners &7>> &cPlugin Disabled!"));//prints disabled message to console
+        getServer().getConsoleSender().sendMessage(Utils.chat("&bCSSpawners &7>> &cPlugin Disabled!"));
     }
 }
