c64d0c68dca1b44de782bebd1ff3785285e582b9
Cosmic-0022 <75631523+Cosmic-0022@users.noreply.github.com>
2021-03-12 08:41:18 +0000

    Update CustomListener.java

--- SilkSpawners/src/main/java/me/cosmic/cosmicssilkspawners/listeners/CustomListener.java
+++ SilkSpawners/src/main/java/me/cosmic/cosmicssilkspawners/listeners/CustomListener.java
@@ -12,20 +12,19 @@ public class CustomListener implements Listener {
 
     @EventHandler
     public void onSpawnerBreakEvent(SpawnerBreakEvent e){
-        CreatureSpawner cs = (CreatureSpawner) e.getSpawner().getState();//just makes it easier to find the spawner
+        CreatureSpawner cs = (CreatureSpawner) e.getSpawner().getState();
 
-        //create a new spawner and set its entity type for the player to have
-        ItemStack spawner_to_give = new ItemStack(Material.SPAWNER);//sets a variable to give a spawner
-        BlockStateMeta meta = (BlockStateMeta) spawner_to_give.getItemMeta();//sets meta to the spawner
-        CreatureSpawner css = (CreatureSpawner) meta.getBlockState();//checks the blockstate
+        ItemStack spawner_to_give = new ItemStack(Material.SPAWNER);
+        BlockStateMeta meta = (BlockStateMeta) spawner_to_give.getItemMeta();
+        CreatureSpawner css = (CreatureSpawner) meta.getBlockState();
 
 
-        css.setSpawnedType(cs.getSpawnedType());//checks what mob gets spawned
-        meta.setBlockState(css);//adds the spawned type
-        meta.setDisplayName("[" + css.getCreatureTypeName() + "] Spawner");//sets the name of the meta
-        spawner_to_give.setItemMeta(meta);//applies the meta value to the spawner
+        css.setSpawnedType(cs.getSpawnedType());
+        meta.setBlockState(css);
+        meta.setDisplayName("[" + css.getCreatureTypeName() + "] Spawner");
+        spawner_to_give.setItemMeta(meta);
 
-        e.getBreaker().getInventory().addItem(spawner_to_give);//adds to the inventory
+        e.getBreaker().getInventory().addItem(spawner_to_give);
 
 
 
